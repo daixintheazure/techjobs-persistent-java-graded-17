9d1acdd745fe133fe439f61edf29bb51d3bbc97a
Mario Dickerson <dai.dickerson@gmail.com>
2023-10-30 18:08:14 -0500

    updated Skill

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(min = 3, message = "Description must be at least 3 characters.")
+    private String description;
+
+    public Skill() {}
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
