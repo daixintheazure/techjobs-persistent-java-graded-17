0c3952e212d6fc4f262bb3d2fda733397d333819
Mario Dickerson <dai.dickerson@gmail.com>
2023-10-30 20:27:26 -0500

    created skill and employer controllers

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,18 @@ import java.util.Optional;
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @RequestMapping("")
+    public String index (Model model) {
+//        Iterable employers;
+//        employers = employerRepository.findAll();
+//        model.addAttribute("employers", employers);
+
+        return "employers";
+    }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
@@ -27,6 +40,7 @@ public class EmployerController {
         if (errors.hasErrors()) {
             return "employers/add";
         }
+        employerRepository.save(newEmployer);
 
         return "redirect:";
     }
@@ -34,7 +48,7 @@ public class EmployerController {
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
@@ -44,4 +58,7 @@ public class EmployerController {
         }
 
     }
+
+
+
 }
