005978a0377f1745d2a92c620fb2c0902779c071
Mario Dickerson <dai.dickerson@gmail.com>
2023-11-01 10:34:01 -0500

    task 3 done

--- queries.sql
+++ queries.sql
@@ -3,5 +3,5 @@
 --Part 2
 select name from employer where location = "St. Louis City";
 --Part 3
-
+drop table job;
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -2,6 +2,8 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,10 +19,14 @@ import java.util.Optional;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
     @RequestMapping("/")
     public String index(Model model) {
 
         model.addAttribute("title", "MyJobs");
+
 
         return "index";
     }
@@ -28,6 +34,7 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
 	model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
@@ -40,6 +47,8 @@ public class HomeController {
 	    model.addAttribute("title", "Add Job");
             return "add";
         }
+        model.addAttribute("employerId", employerRepository.findAll());
+        model.addAttribute("employers", employerRepository.findById(employerId));
 
         return "redirect:";
     }
